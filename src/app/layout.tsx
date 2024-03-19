86879b623cf541d20e61cf5f6d14c3602bc8f06f
sqbislam <sqbislam101@gmail.com>
2024-03-20 03:22:15 +0600

    fix: default lorem ipsum removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,9 +64,10 @@ export default function RootLayout({
               <div className='max-w-xl p-4 rounded-lg w-full'>
                 <h1 className='text-3xl font-bold mb-4'>Illustration AI</h1>
                 <p className='text-grawy-600 mb-6'>
-                  Welcome to our AI application. Lorem ipsum dolor sit amet,
-                  consectetur adipiscing elit. Sed do eiusmod tempor incididunt
-                  ut labore et dolore magna aliqua.
+                  Welcome to our AI application. An open source free platform to
+                  generate beautiful illustrations. Generate visually striking
+                  illustrations from your text. Create beautiful illustrations
+                  with just a few clicks and download them!
                 </p>
 
                 <div className='flex justify-center w-full'>{children}</div>
